c45b127d9467a12b8edcbb4eb8e043ba2eb8d89a
wndnsud-ui <wndnsud@gmail.com>
2026-07-31 11:54:27 +0900

    Initialize Flask project

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask Team Project!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
